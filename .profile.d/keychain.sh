3f12d4adde50e49f8fc4a9bfb756397581d005e3
Matt Fields <mafields@0x6d617474.net>
2020-06-21 05:17:08 -0400

    Update keychain config to use GPG

--- .profile.d/keychain.sh
+++ .profile.d/keychain.sh
@@ -1,11 +1,19 @@
 # Keychain setup
-if command -v keychain >/dev/null 2>&1 ; then
+if command -v keychain &>/dev/null; then
     eval "$(TERM=${TERM:-ansi} keychain \
-        --eval --ignore-missing --quiet id_dsa id_rsa id_ecsda id_ed25519 --agents ssh)"
+        --eval --ignore-missing --quiet \
+        --inherit local --noask --agents gpg)"
 
     # Set and export TTY/GPG_TTY for interactive shells
     if [ -t 0 ] ; then
-        GPG_TTY=$(tty)
-        export GPG_TTY
+        export GPG_TTY=$(tty)
     fi
 fi
+
+# Configure the gpg-agent started by keychain
+if command -v gpg-connect-agent &> /dev/null; then
+  gpg-connect-agent updatestartuptty /bye > /dev/null
+fi
+
+# Use gpg-agent for SSH
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
